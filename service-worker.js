b4fe8a17a7037088adddb73f892e1ad9a1aecd09
Samba Sene <celerity5s9@gmail.com>
2025-03-16 15:13:13 +0000

    cash some stuff here and there and

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,24 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/favicon.png',
+  '/ampoule_led_e27.jpg',
+  '/ampoule_led_b22.jpg',
+  '/spot_led_encastrable.jpg',
+  '/applique_murale_led.jpg',
+  '/led_escalier_interieur.jpg',
+  '/lampe_exterieur_led.jpg',
+  '/projecteur_detecteur_mouvement.jpg',
+  '/douille_ampoule.jpg',
+  '/interrupteur_simple.jpg',
+  '/interrupteur_va_et_vient.jpg',
+  '/interrupteur_double_allumage.jpg',
+  '/bouton_poussoir.jpg',
+  '/prise_tv_sonnerie.jpg',
+  '/prise_courant_pt2.jpg',
+  '/minuterie.jpg',
+  '/disjoncteur_dismatic.jpg',
+  '/telerupteur.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'
   // Add other assets you want to cache
 ];
 
